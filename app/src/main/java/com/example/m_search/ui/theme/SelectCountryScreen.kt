a83b0ffbb1e637c37bab157e96bd48f7186e09ad
BuisL <l.bittau@student.vu.nl>
2024-06-20 02:22:44 +0200

    Added Mode Selection Screen

--- app/src/main/java/com/example/m_search/ui/theme/SelectCountryScreen.kt
+++ app/src/main/java/com/example/m_search/ui/theme/SelectCountryScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -88,7 +89,7 @@ fun SelectCountryScreen(
                 ){
                     Text(text = stringResource(R.string.app_next))
                 }
-                Button(
+                OutlinedButton(
                     modifier = Modifier.widthIn(min = 250.dp),
                     onClick = {}
                 ){
--- app/src/main/java/com/example/m_search/ui/theme/SelectModeScreen.kt
+++ app/src/main/java/com/example/m_search/ui/theme/SelectModeScreen.kt
@@ -1,11 +1,17 @@
 package com.example.m_search.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,16 +30,14 @@ fun SelectModeScreen(
 ) {
     Column(
         modifier = modifier.padding(top = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Spacer(modifier = Modifier.height(10.dp))
         Text(
             text = stringResource(R.string.mode_selection_title),
             style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier.padding(bottom = 0.dp)
         )
-
-        Spacer(modifier = Modifier.height(30.dp))
 
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -44,6 +48,39 @@ fun SelectModeScreen(
                 style = MaterialTheme.typography.bodyLarge,
                 textAlign = TextAlign.Center
             )
+        }
+
+        Row() {
+            Button(
+                modifier = Modifier
+                    .heightIn(min = 100.dp)
+                    .weight(1f)
+                    .padding(10.dp),
+                onClick = {},
+            ){
+                Text(
+                    text = stringResource(R.string.mode_manual),
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+            Button(
+                modifier = Modifier
+                    .heightIn(min = 100.dp)
+                    .weight(1f)
+                    .padding(10.dp),
+                onClick = {}
+            ){
+                Text(
+                    text = stringResource(R.string.mode_scan),
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+        }
+        OutlinedButton(
+            modifier = Modifier.widthIn(min = 250.dp),
+            onClick = {}
+        ){
+            Text(text = stringResource(R.string.app_back))
         }
     }
 }
